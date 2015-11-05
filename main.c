fa62bc1fc856e39a538289299ab3ea00af05ed29
pawelkulig <pawelkulig1@gmail.com>
2015-11-05 14:00:31 +0100

    added getting connection to node

--- main.c
+++ main.c
@@ -17,7 +17,9 @@ struct con
 
 void get_data_from_file(struct nodes *n_rcv, struct con *c_rcv);
 double dist(double x1, double y1, double x2, double y2);
-
+void init_table(double *r_dist, double *r_nnumber, double *r_nDone, double *r_Done, int s);
+void init_con(struct con *r_con);
+int get_connection(int node_number, int con_number, struct con *r_con);
 
 int main()
 {
@@ -25,11 +27,63 @@ int main()
 
     struct nodes node[6];
     struct con conn[6*5];
+    init_con(conn); //set every element to -1
     get_data_from_file(node, conn);
     //printf("%f", dist(-4,4,-5,5));
     //printf("%f", node[1].x);
 
+    double distance[6]; //distance between nodes
+    double node_number[6]; //number of node
+    double nDone[6]; //nodes not calculated
+    double Done[6]; //nodes calculated
+    init_table(distance, node_number, nDone, Done, 6);
+
+    printf("%i", get_connection(4,2,conn)); //works!
+
+
+
+
     return 0;
+}
+
+int get_connection(int node_number, int con_number, struct con *r_con)
+{
+    int i=0;
+    int c=0;
+    for(i;i<6*(6-1);i++)
+    {
+        if(r_con[i].b == node_number || r_con[i].e == node_number)
+        {
+            //printf("%i", i);
+
+            if(c==con_number) return i;
+            c++; //we should reach this standard and leave c :)
+        }
+    }
+    return -1;
+}
+
+void init_table(double *r_dist, double *r_nnumber, double *r_nDone, double *r_Done, int s)
+{
+    int i=0;
+    for(i;i<s;i++)
+    {
+        r_dist[i] = 100000;
+        r_nnumber[i] = -1;
+        r_nDone[i] = i;
+        r_Done[i] = -1;
+    }
+    r_dist[0]=0; //always 0 to itself
+}
+
+void init_con(struct con *r_con)
+{
+    int i=0;
+    for(i; i<6*(6-1);i++)
+    {
+        r_con[i].b=-1;
+        r_con[i].e=-1;
+    }
 }
 
 double dist(double x1, double y1, double x2, double y2)
